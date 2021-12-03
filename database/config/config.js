34fe329ac84d1f64b430e826af472ed21e58b31d
LF236 <fenandorodiguez236@gmail.com>
2021-12-03 11:02:27 -0600

    Se agrega docker para la ejecución

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     "username": process.env.MYSQL_USER,
     "password": process.env.MYSQL_USER_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
-    "host": "127.0.0.1",
+    "host": "dbExamen",
     "dialect": "mysql"
   },
   "test": {
